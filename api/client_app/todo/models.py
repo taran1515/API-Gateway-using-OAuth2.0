bd28066681e26e8b95e5b9f8499b23ea1a6e4ce8
Taranjeet Singh <singhtaran1515@gmail.com>
2020-04-26 13:13:51 +0530

    Added Coverage Report and Screenshots

--- api/client_app/todo/models.py
+++ api/client_app/todo/models.py
@@ -10,3 +10,6 @@ class Todo(models.Model):
     todo = models.CharField(max_length=100, null=False,
                             help_text="This field is required")
     done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.todo
